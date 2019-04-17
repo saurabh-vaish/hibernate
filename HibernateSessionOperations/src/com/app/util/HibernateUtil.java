e75c8de84604dc451fc415610e83d75c0acb5fa3
Incredible Srv <saurabh.vaish1993@gmail.com>
2019-04-17 19:25:06 +0530

    Update HibernateUtil.java

--- HibernateSessionOperations/src/com/app/util/HibernateUtil.java
+++ HibernateSessionOperations/src/com/app/util/HibernateUtil.java
@@ -7,6 +7,8 @@ public class HibernateUtil {
 
 	// since SessionFactory is heavy object so we are using this class so that this obj can be load only one time 
 	
+	// SessionFactory will load driver , create connection and prepare statements  
+	
 	private static SessionFactory sf=null;
 	
 	static {
