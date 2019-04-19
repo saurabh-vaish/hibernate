074816df993091665f562a7ae53ade13d64f2401
Incredible Srv <saurabh.vaish1993@gmail.com>
2019-04-19 23:21:34 +0530

    Example of ComponenetMapping

---
+++ ComponentMapping/src/com/app/model/Address.java
@@ -0,0 +1,42 @@
+package com.app.model;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+
+@Embeddable            // used at child class level it says do not use this class as independent table 
+public class Address {
+
+	@Column(name="ahno")
+	private String hno;
+	@Column(name="aloc")
+	private String loc;
+	
+	public Address() {
+		super();
+	}
+
+	public String getHno() {
+		return hno;
+	}
+
+	public void setHno(String hno) {
+		this.hno = hno;
+	}
+
+	public String getLoc() {
+		return loc;
+	}
+
+	public void setLoc(String loc) {
+		this.loc = loc;
+	}
+
+	@Override
+	public String toString() {
+		return "Address [hno=" + hno + ", loc=" + loc + "]";
+	}
+	
+	
+	
+	
+}
---
+++ ComponentMapping/src/com/app/test/TestSave.java
@@ -0,0 +1,57 @@
+package com.app.test;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import com.app.model.Address;
+import com.app.model.Employee;
+import com.app.util.HibernateUtil;
+
+public class TestSave {
+
+	public static void main(String[] args) {
+		
+		Transaction tx= null;
+		
+		try(Session ses = HibernateUtil.getSf().openSession())
+		{
+			tx = ses.beginTransaction();
+			
+			// make child class object first
+			
+			Address a = new Address();
+			a.setHno("M-291");
+			a.setLoc("HYD");
+			
+			// parent class object 
+			Employee e = new Employee();
+			e.setEmpId(10);
+			e.setEmpName("swetara");
+			e.setAddr(a);
+			
+			
+			//inserting data 
+			ses.save(e);
+			
+			System.out.println("data saved");
+			
+			
+
+			tx.commit();
+			
+			// fetching data 
+			Employee emp = ses.get(Employee.class, 10);   //get does not need transaction
+			
+			System.out.println(emp);
+			
+			
+		}
+		catch (Exception e) {
+			tx.rollback();
+			e.printStackTrace();
+		}
+		
+	}
+	
+	
+}
